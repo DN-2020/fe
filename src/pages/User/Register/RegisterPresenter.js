df290dbea980908ba5cb4c57f211796bfc7640d0
moonchanghee <56143212+moonchanghee@users.noreply.github.com>
2020-10-05 17:11:23 +0900

    login, register add2

--- src/pages/User/Register/RegisterPresenter.js
+++ src/pages/User/Register/RegisterPresenter.js
@@ -1,52 +1,94 @@
 import React from 'react';
-import {Link} from "react-router-dom"
-import {Card , Button , Layout , Input} from 'antd';
+import { Link } from 'react-router-dom';
+import { Card, Button, Layout, Input } from 'antd';
 import PropTypes from 'prop-types';
-import {NodeExpandOutlined, UserOutlined} from '@ant-design/icons'
-const { Header,Content } = Layout;
+import { NodeExpandOutlined, UserOutlined } from '@ant-design/icons';
+const { Header, Content } = Layout;
 /*
 npm install --save @ant-design/icons
-*/ 
+*/
 
-
-const RegisterPresenter = props => {
-    return (
-        <>
-        <Layout>
-        <Header>
-        </Header>
+const RegisterPresenter = (props) => {
+  return (
+    <>
+      <Layout>
         <Content>
-        <Card style={{ width: 500 , height : 600, marginLeft : "auto" , marginRight :"auto"  , marginTop : "2%" , marginBottom : "10%"}}>
-        <div style ={{textAlign : "center" , marginTop : "10%"}}>
-        <Header>
-        <p style ={{fontSize : 30 , color :"#ffffff"}}>회원가입</p>
-        </Header> 
-        <div style ={{marginLeft : "10%" , marginRight : "10%" , marginBottom : "10%" , marginTop : "10%"}}>
-        <Input style={{border : "none" , borderBottom : "1px solid"}} placeholder="UserName" prefix={<UserOutlined />}/>  
-        <br/><br/>
-        <Input style={{border : "none" , borderBottom : "1px solid"}} placeholder="Id" prefix={<UserOutlined />} />
-        <br/><br/>
-        <Input.Password placeholder="pwd" style={{border : "none" , borderBottom : "1px solid"}} />
-        <br/><br/>
-        <Input placeholder="Address"  style={{border : "none" , borderBottom : "1px solid"}}/>  
-        <br/><br/>
-        <Input placeholder="Phone" style={{border : "none" , borderBottom : "1px solid"}}/>  
-        <br/><br/>
-        <Input placeholder="Email" type = "email"  style={{border : "none" , borderBottom : "1px solid"}}/>  
-        <br/><br/>
-        </div>
-        <Button>SignUp</Button>
-        <Button style ={{marginLeft : "15%"}}><Link to = "/user/login">Cancel</Link></Button>
-        </div>
-      </Card>
-      </Content>
+          <Card style={style.cardStyle}>
+            <Header>
+              <p style={style.Pstyle}>회원가입</p>
+            </Header>
+            <div style={style.DivStyle}>
+              <Input
+                style={style.InputStyle}
+                placeholder="UserName"
+                prefix={<UserOutlined />}
+              />
+              <br />
+              <br />
+              <Input
+                style={style.InputStyle}
+                placeholder="Id"
+                prefix={<UserOutlined />}
+              />
+              <br />
+              <br />
+              <Input.Password placeholder="pwd" style={style.InputStyle} />
+              <br />
+              <br />
+              <Input placeholder="address" style={style.InputStyle} />
+              <br />
+              <br />
+              <Input placeholder="Phone" style={style.InputStyle} />
+              <br />
+              <br />
+              <Input
+                placeholder="Email"
+                type="email"
+                style={style.InputStyle}
+              />
+              <br />
+              <br />
+            </div>
+            <Button>SignUp</Button>
+            <Button style={style.CancelStyle}>
+              <Link to="/user/login">Cancel</Link>
+            </Button>
+          </Card>
+        </Content>
       </Layout>
-        </>
-    );
+    </>
+  );
 };
 
-RegisterPresenter.propTypes = {
-    
+RegisterPresenter.propTypes = {};
+
+const style = {
+  Pstyle: {
+    fontSize: 30,
+    color: '#ffffff',
+  },
+  cardStyle: {
+    width: 500,
+    height: 600,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: '2%',
+    marginBottom: '10%',
+    textAlign: 'center',
+  },
+  InputStyle: {
+    border: 'none',
+    borderBottom: '1px solid',
+  },
+  DivStyle: {
+    marginLeft: '10%',
+    marginRight: '10%',
+    marginBottom: '10%',
+    marginTop: '10%',
+  },
+  CancelStyle: {
+    marginLeft: '15%',
+  },
 };
 
 export default RegisterPresenter;
--- src/pages/User/login/LoginPresenter.js
+++ src/pages/User/login/LoginPresenter.js
@@ -1,39 +1,62 @@
 import React from 'react';
-import {Card , Button ,Layout , Input} from 'antd';
+import { Card, Button, Layout, Input } from 'antd';
 import PropTypes from 'prop-types';
-import {Link} from "react-router-dom"
-const { Header,Content } = Layout;
+import { Link } from 'react-router-dom';
+const { Header, Content } = Layout;
 
-const LoginPresenter = props => {
-    return (
-        <>
-        <Layout>
-       <Header>
-       s
-       </Header>
-       <Content>
-       <Card style={{ width: 500 , height : 500, marginLeft : "auto" , marginRight :"auto"  , marginTop : "5%" , marginBottom : "20%"}}>
-       <div style ={{textAlign : "center" , marginTop : "10%"}}>
-       <Header><p style ={{fontSize : 30 , color : "#ffffff"}}>로그인</p>        </Header>
-       <div style ={{marginLeft : "10%" , marginRight : "10%" , marginBottom : "20%" , marginTop : "10%"}}>
-       <Input placeholder="ID" style={{border : "none" , borderBottom : "1px solid"}}/> 
-       <br/><br/>
-       <Input.Password placeholder="password" style={{border : "none" , borderBottom : "1px solid"}}  />
-       </div>
-       <Button>Login</Button>
-       <Button style ={{marginLeft : "10%"}}><Link to ="/user/register">SignUp</Link></Button>
-       </div>
-     </Card>
-     </Content>
-     </Layout>
-
-
-      </>
-      );
+const LoginPresenter = (props) => {
+  return (
+    <>
+      <Layout>
+        <Content>
+          <Card style={style.cardStyle}>
+            <div style={{ textAlign: 'center', marginTop: '10%' }}>
+              <Header>
+                <p style={{ fontSize: 30, color: '#ffffff' }}>로그인</p>{' '}
+              </Header>
+              <div style={style.InputDivStyle}>
+                <Input
+                  placeholder="ID"
+                  style={{ border: 'none', borderBottom: '1px solid' }}
+                />
+                <br />
+                <br />
+                <Input.Password
+                  placeholder="password"
+                  style={{ border: 'none', borderBottom: '1px solid' }}
+                />
+              </div>
+              <Button>Login</Button>
+              <Button style={style.SignUpButtonStyle}>
+                <Link to="/user/register">SignUp</Link>
+              </Button>
+            </div>
+          </Card>
+        </Content>
+      </Layout>
+    </>
+  );
 };
 
-LoginPresenter.propTypes = {
-    
-};
+LoginPresenter.propTypes = {};
 
+const style = {
+  cardStyle: {
+    width: 500,
+    height: 500,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: '5%',
+    marginBottom: '20%',
+  },
+  InputDivStyle: {
+    marginLeft: '10%',
+    marginRight: '10%',
+    marginBottom: '20%',
+    marginTop: '10%',
+  },
+  SignUpButtonStyle: {
+    marginLeft: '10%',
+  },
+};
 export default LoginPresenter;
