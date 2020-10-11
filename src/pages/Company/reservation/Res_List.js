c240743c1479a9edd12c20974877425cc8f8b25a
rbsghks2 <rbsghks01@gmail.com>
2020-10-11 18:48:07 +0900

    add Res_List

--- src/pages/Company/reservation/Res_List.js
+++ src/pages/Company/reservation/Res_List.js
@@ -1,10 +1,47 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Layout } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
+
 
 const Res_List=(props)=> {
     return (
         <div>
-            
+              <>
+    <Layout>
+      <Header>Header</Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
+
+    <Layout>
+      <Header>Header</Header>
+      <Layout>
+        <Sider>Sider</Sider>
+        <Content>Content</Content>
+      </Layout>
+      <Footer>Footer</Footer>
+    </Layout>
+
+    <Layout>
+      <Header>Header</Header>
+      <Layout>
+        <Content>Content</Content>
+        <Sider>Sider</Sider>
+      </Layout>
+      <Footer>Footer</Footer>
+    </Layout>
+
+    <Layout>
+      <Sider>Sider</Sider>
+      <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </Layout>
+  </>            
         </div>
     )
 }
