17e7480cf9b0a5f7de8a0827f452f152ecfbc554
rbsghks2 <rbsghks01@gmail.com>
2020-11-21 11:26:55 +0900

    add

--- src/api/Admin_Reservation.js
+++ src/api/Admin_Reservation.js
@@ -1,7 +1,7 @@
-import { reviewAPI } from '.';
-import { backend_url } from '../Utils';
-import { ApiManager } from '../Utils';
-const $http = new ApiManager();
+import { reviewAPI } from '.'
+import { backend_url } from '../Utils'
+import { ApiManager } from '../Utils'
+const $http = new ApiManager()
 export default {
   /**
    * @title findAll
@@ -9,12 +9,12 @@ export default {
    */
   getReservation: async () => {
     // const url = `${backend_url}/v1/admin/reservation`;
-    const url = `${backend_url}/v1/company/1/reservation`;
+    const url = `${backend_url}/v1/company/1/reservation`
     // const url = `${backend_url}/v1/admin/reservation`;
     // const url = `${backend_url}/v1/admin/reservation`;
 
     // const url = `http://192.168.64.94:8080/v1/goodsdetail/5/review`;
 
-    return await $http.get(url);
+    return await $http.get(url)
   },
-};
+}
--- src/pages/GyunCompany/ReviewList/ReviewListPresenter.js
+++ src/pages/GyunCompany/ReviewList/ReviewListPresenter.js
@@ -1,36 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { backend_url } from '../../../Utils';
-import PropTypes from 'prop-types';
-import Axios from 'axios';
-import { Layout, Menu, Button } from 'antd';
-import { Table } from 'antd';
-import { useHistory } from 'react-router-dom';
-import { get } from 'js-cookie';
-import { UploadOutlined, InboxOutlined } from '@ant-design/icons';
-import PostCodeInput from '../../../component/PostCodeInput';
-const { Header, Footer, Sider, Content } = Layout;
+import React, { useState, useEffect } from 'react'
+import { backend_url } from '../../../Utils'
+import PropTypes from 'prop-types'
+import Axios from 'axios'
+import { Layout, Menu, Button } from 'antd'
+import { Table } from 'antd'
+import { useHistory } from 'react-router-dom'
+import { get } from 'js-cookie'
+import { UploadOutlined, InboxOutlined } from '@ant-design/icons'
+
+const { Header, Footer, Sider, Content } = Layout
 let config = {
   headers: {
     'Access-Control-Allow-Origin': true,
     // 'Set-Cookie': Axios.defaults.headers.common['Set-Cookie'],
     Authorization: get('accessToken'),
   },
-};
+}
 
 const ReviewListPresenter = (props) => {
-  let arr1 = [1];
-  let history = useHistory();
-  const [reviewNo, setReviewNo] = useState();
-  const [key, setKey] = useState('');
-  const [reviews, setreviews] = useState([]);
+  let arr1 = [1]
+  let history = useHistory()
+  const [reviewNo, setReviewNo] = useState()
+  const [key, setKey] = useState('')
+  const [reviews, setreviews] = useState([])
   const handleKey = (e) => {
     Axios.get(
-      `https://${backend_url}/v1/goods/${
-        props.goods_seq[e.key.replace(/\item_/g, '')]
-      }/review`,
+      `https://${backend_url}/v1/goods/${props.goods_seq[e.key.replace(/\item_/g, '')]}/review`,
       config
     ).then((e) => {
-      console.log(e);
+      console.log(e)
       if (e.data.data != null) {
         arr1 = e.data.data.map((e) => {
           return {
@@ -39,22 +37,22 @@ const ReviewListPresenter = (props) => {
             review: e.review_content,
             starRate: e.review_grade,
             end: e.reservation_end,
-          };
-        });
-        setreviews(arr1);
+          }
+        })
+        setreviews(arr1)
       } else {
-        alert('리뷰가 없습니다');
+        alert('리뷰가 없습니다')
       }
-    });
-  };
+    })
+  }
 
-  const [currentPage, setCurrentPage] = useState(1); //시작
-  const [postPerPage] = useState(5); //개수
-  let indexOfLastPost = currentPage * postPerPage; //페이지에 마지막 데이터 인덱스/3
-  let indexOfFirstPost = indexOfLastPost - postPerPage; //페이지 첫번째 데이터 인덱스/0
-  let datas = reviews.slice(indexOfFirstPost, indexOfLastPost); //  1*5 last = 5 ,,, first = 0/0,3/
-  let count = []; //  2*5 last = 10 ,,, first = 5
-  let total = Math.ceil(reviews.length / postPerPage); //버튼count
+  const [currentPage, setCurrentPage] = useState(1) //시작
+  const [postPerPage] = useState(5) //개수
+  let indexOfLastPost = currentPage * postPerPage //페이지에 마지막 데이터 인덱스/3
+  let indexOfFirstPost = indexOfLastPost - postPerPage //페이지 첫번째 데이터 인덱스/0
+  let datas = reviews.slice(indexOfFirstPost, indexOfLastPost) //  1*5 last = 5 ,,, first = 0/0,3/
+  let count = [] //  2*5 last = 10 ,,, first = 5
+  let total = Math.ceil(reviews.length / postPerPage) //버튼count
 
   const column = [
     {
@@ -97,21 +95,21 @@ const ReviewListPresenter = (props) => {
       dataIndex: 'review',
       render: (text) => <div>{text}</div>,
     },
-  ];
+  ]
 
   for (let i = 1; i <= total; i++) {
-    count.push(i);
+    count.push(i)
   }
   const handleRow = (data) => {
-    console.log(data);
+    console.log(data)
     // history.push(`/company/review/${data.seq}`);
     history.push({
       pathname: `/company/review/${data.seq}`,
       // search: `query=${data.seq}`,
       state: { detail: data },
-    });
-  };
-  console.log(reviewNo);
+    })
+  }
+  console.log(reviewNo)
   return (
     <>
       <Header style={style.headerLayout}>
@@ -121,9 +119,7 @@ const ReviewListPresenter = (props) => {
         <div className="reviewContainer" style={style.reviewContainer}>
           <Layout className="reviewContent">
             <Sider>
-              <h4 style={{ color: 'white', fontSize: '20px' }}>
-                상품({props.goods_nm.length})
-              </h4>
+              <h4 style={{ color: 'white', fontSize: '20px' }}>상품({props.goods_nm.length})</h4>
               {/**           <Menu theme="dark" selectedKeys={key} mode="inline">
                 {props.goods_nm != null
                   ? menuData.map((item) => {
@@ -140,7 +136,7 @@ const ReviewListPresenter = (props) => {
               <Menu theme="dark" selectedKeys={key} mode="inline">
                 {props.goods_nm != null
                   ? props.goods_nm.map((e) => {
-                      return <Menu.Item onClick={handleKey}>{e}</Menu.Item>;
+                      return <Menu.Item onClick={handleKey}>{e}</Menu.Item>
                     })
                   : 'noData'}
               </Menu>
@@ -150,7 +146,7 @@ const ReviewListPresenter = (props) => {
                 onRow={(record, rowIndex) => {
                   return {
                     onClick: (e) => handleRow(record, rowIndex),
-                  };
+                  }
                 }}
                 columns={column}
                 dataSource={datas}
@@ -166,10 +162,10 @@ const ReviewListPresenter = (props) => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-ReviewListPresenter.propTypes = {};
+ReviewListPresenter.propTypes = {}
 const style = {
   headerLayout: {
     background: '#fff',
@@ -198,5 +194,5 @@ const style = {
     width: '100%',
     height: '100%',
   },
-};
-export default ReviewListPresenter;
+}
+export default ReviewListPresenter
